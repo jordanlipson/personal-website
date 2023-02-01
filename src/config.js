6e00ea52bca76b6b7d2432a7dc4a9ccdc862f9d5
Jordan Lipson <lipsonjordan@gmail.com>
2023-02-01 18:03:54 -0500

    Added new pages

---
+++ src/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    email: 'lipsonjordan@gmail.com',
+
+    socialMedia: [
+        {
+            name: 'Instagram',
+            url: 'https://www.instagram.com/jolipson/',
+        },
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/jordan-lipson/',
+        },
+        {
+            name: 'Medium',
+            url: 'https://medium.com/@jolipson',
+        },
+    ],
+
+    colors: {
+        green: '#0a3200',
+    },
+};
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    html {
+        box-sizing: border-box;
+        width: 100%;
+        scroll-behavior: smooth;
+    }
+
+`;
+
+export default GlobalStyle;
