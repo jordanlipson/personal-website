2bac79311c59f04cf1e6452897e00bb84cd3c3a4
Jordan Lipson <lipsonjordan@gmail.com>
2023-03-02 19:01:39 -0500

    Added menu component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Head } from './head';
 export { default as Layout } from './layout';
 export { default as Nav } from './nav';
+export { default as Menu } from './menu';
 export { default as Footer } from './footer';
 export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
